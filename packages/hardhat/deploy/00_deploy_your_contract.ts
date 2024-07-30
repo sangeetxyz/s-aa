6e52074469b74164218241d3d0cfc7117fbb9137
Sangeet Banerjee <sangeetbanerjee777@gmail.com>
2024-07-30 19:09:45 +0530

    commented out unused var

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { Contract } from "ethers";
+// import { Contract } from "ethers";
 
 /**
  * Deploys a contract named "YourContract" using the deployer account and
@@ -33,7 +33,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const AAAuthToken = await hre.ethers.getContract<Contract>("AAAuthToken", deployer);
+  // const AAAuthToken = await hre.ethers.getContract<Contract>("AAAuthToken", deployer);
   // console.log("👋 Initial greeting:", await AAAuthToken.greeting());
 };
 
